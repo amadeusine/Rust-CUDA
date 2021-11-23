c12cc3156a58a9eb0d75211147ff03a43966eb3e
amadeusine <amadeusine@users.noreply.github.com>
2021-11-23 19:51:39 +0000

    deploy: 23299343a2b96126196ef265e79c25667048c544

--- examples/cuda/cpu/path_tracer/build.rs
+++
@@ -1,8 +0,0 @@
-use cuda_builder::CudaBuilder;
-
-fn main() {
-    CudaBuilder::new("../../gpu/path_tracer_gpu")
-        .copy_to("../../resources/kernels.ptx")
-        .build()
-        .unwrap();
-}
